5f6e642f2a7f9e35c1cdbf68cd3a3085ec73f0c3
P4 <P4@DESKTOP-0QL5K7M>
2021-09-30 13:55:00 -0400

    Add project files.

---
+++ Cash regestir/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace Cash_regestir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PlainBurger = new System.Windows.Forms.Label();
+            this.CheeseBurger = new System.Windows.Forms.Label();
+            this.BaconCheeseBurger = new System.Windows.Forms.Label();
+            this.box1 = new System.Windows.Forms.TextBox();
+            this.box2 = new System.Windows.Forms.TextBox();
+            this.box3 = new System.Windows.Forms.TextBox();
+            this.TaxLabel = new System.Windows.Forms.Label();
+            this.TotalPriceLabel = new System.Windows.Forms.Label();
+            this.PayHere = new System.Windows.Forms.Label();
+            this.PayBox = new System.Windows.Forms.TextBox();
+            this.ChangeCalculator = new System.Windows.Forms.Button();
+            this.ReciptBotton = new System.Windows.Forms.Button();
+            this.BuyButton = new System.Windows.Forms.Button();
+            this.subtotalLabel = new System.Windows.Forms.Label();
+            this.subtotalOutput = new System.Windows.Forms.Label();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PlainBurger
+            // 
+            this.PlainBurger.AutoSize = true;
+            this.PlainBurger.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlainBurger.Location = new System.Drawing.Point(93, 23);
+            this.PlainBurger.Name = "PlainBurger";
+            this.PlainBurger.Size = new System.Drawing.Size(152, 29);
+            this.PlainBurger.TabIndex = 0;
+            this.PlainBurger.Text = "Plain Burger ";
+            // 
+            // CheeseBurger
+            // 
+            this.CheeseBurger.AutoSize = true;
+            this.CheeseBurger.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheeseBurger.Location = new System.Drawing.Point(93, 82);
+            this.CheeseBurger.Name = "CheeseBurger";
+            this.CheeseBurger.Size = new System.Drawing.Size(182, 29);
+            this.CheeseBurger.TabIndex = 1;
+            this.CheeseBurger.Text = "Cheese Burger ";
+            // 
+            // BaconCheeseBurger
+            // 
+            this.BaconCheeseBurger.AutoSize = true;
+            this.BaconCheeseBurger.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BaconCheeseBurger.Location = new System.Drawing.Point(83, 138);
+            this.BaconCheeseBurger.Name = "BaconCheeseBurger";
+            this.BaconCheeseBurger.Size = new System.Drawing.Size(256, 29);
+            this.BaconCheeseBurger.TabIndex = 2;
+            this.BaconCheeseBurger.Text = "Bacon Cheese Burger ";
+            // 
+            // box1
+            // 
+            this.box1.Location = new System.Drawing.Point(239, 32);
+            this.box1.Name = "box1";
+            this.box1.Size = new System.Drawing.Size(100, 20);
+            this.box1.TabIndex = 3;
+            // 
+            // box2
+            // 
+            this.box2.Location = new System.Drawing.Point(265, 91);
+            this.box2.Name = "box2";
+            this.box2.Size = new System.Drawing.Size(100, 20);
+            this.box2.TabIndex = 4;
+            // 
+            // box3
+            // 
+            this.box3.Location = new System.Drawing.Point(332, 147);
+            this.box3.Name = "box3";
+            this.box3.Size = new System.Drawing.Size(100, 20);
+            this.box3.TabIndex = 5;
+            // 
+            // TaxLabel
+            // 
+            this.TaxLabel.AutoSize = true;
+            this.TaxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TaxLabel.Location = new System.Drawing.Point(93, 252);
+            this.TaxLabel.Name = "TaxLabel";
+            this.TaxLabel.Size = new System.Drawing.Size(59, 29);
+            this.TaxLabel.TabIndex = 7;
+            this.TaxLabel.Text = "Tax ";
+            this.TaxLabel.Click += new System.EventHandler(this.Tax_Click);
+            // 
+            // TotalPriceLabel
+            // 
+            this.TotalPriceLabel.AutoSize = true;
+            this.TotalPriceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalPriceLabel.Location = new System.Drawing.Point(93, 281);
+            this.TotalPriceLabel.Name = "TotalPriceLabel";
+            this.TotalPriceLabel.Size = new System.Drawing.Size(136, 29);
+            this.TotalPriceLabel.TabIndex = 8;
+            this.TotalPriceLabel.Text = "Total Price ";
+            // 
+            // PayHere
+            // 
+            this.PayHere.AutoSize = true;
+            this.PayHere.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PayHere.Location = new System.Drawing.Point(93, 310);
+            this.PayHere.Name = "PayHere";
+            this.PayHere.Size = new System.Drawing.Size(112, 29);
+            this.PayHere.TabIndex = 9;
+            this.PayHere.Text = "Pay Here";
+            // 
+            // PayBox
+            // 
+            this.PayBox.Location = new System.Drawing.Point(211, 319);
+            this.PayBox.Name = "PayBox";
+            this.PayBox.Size = new System.Drawing.Size(100, 20);
+            this.PayBox.TabIndex = 10;
+            // 
+            // ChangeCalculator
+            // 
+            this.ChangeCalculator.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChangeCalculator.Location = new System.Drawing.Point(88, 345);
+            this.ChangeCalculator.Name = "ChangeCalculator";
+            this.ChangeCalculator.Size = new System.Drawing.Size(195, 31);
+            this.ChangeCalculator.TabIndex = 11;
+            this.ChangeCalculator.Text = "Change Calculator";
+            this.ChangeCalculator.UseVisualStyleBackColor = true;
+            this.ChangeCalculator.Click += new System.EventHandler(this.ChangeCalculator_Click);
+            // 
+            // ReciptBotton
+            // 
+            this.ReciptBotton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReciptBotton.Location = new System.Drawing.Point(85, 407);
+            this.ReciptBotton.Name = "ReciptBotton";
+            this.ReciptBotton.Size = new System.Drawing.Size(160, 31);
+            this.ReciptBotton.TabIndex = 13;
+            this.ReciptBotton.Text = "Print Recipt";
+            this.ReciptBotton.UseVisualStyleBackColor = true;
+            // 
+            // BuyButton
+            // 
+            this.BuyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BuyButton.Location = new System.Drawing.Point(98, 185);
+            this.BuyButton.Name = "BuyButton";
+            this.BuyButton.Size = new System.Drawing.Size(131, 35);
+            this.BuyButton.TabIndex = 14;
+            this.BuyButton.Text = "Buy Burger";
+            this.BuyButton.UseVisualStyleBackColor = true;
+            this.BuyButton.Click += new System.EventHandler(this.BuyButton_Click);
+            // 
+            // subtotalLabel
+            // 
+            this.subtotalLabel.AutoSize = true;
+            this.subtotalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtotalLabel.Location = new System.Drawing.Point(93, 223);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(101, 29);
+            this.subtotalLabel.TabIndex = 15;
+            this.subtotalLabel.Text = "Subtotal";
+            // 
+            // subtotalOutput
+            // 
+            this.subtotalOutput.AutoSize = true;
+            this.subtotalOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtotalOutput.Location = new System.Drawing.Point(210, 223);
+            this.subtotalOutput.Name = "subtotalOutput";
+            this.subtotalOutput.Size = new System.Drawing.Size(37, 29);
+            this.subtotalOutput.TabIndex = 16;
+            this.subtotalOutput.Text = "---";
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.AutoSize = true;
+            this.changeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeLabel.Location = new System.Drawing.Point(93, 375);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(97, 29);
+            this.changeLabel.TabIndex = 17;
+            this.changeLabel.Text = "Change";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 541);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.subtotalOutput);
+            this.Controls.Add(this.subtotalLabel);
+            this.Controls.Add(this.BuyButton);
+            this.Controls.Add(this.ReciptBotton);
+            this.Controls.Add(this.ChangeCalculator);
+            this.Controls.Add(this.PayBox);
+            this.Controls.Add(this.PayHere);
+            this.Controls.Add(this.TotalPriceLabel);
+            this.Controls.Add(this.TaxLabel);
+            this.Controls.Add(this.box3);
+            this.Controls.Add(this.box2);
+            this.Controls.Add(this.box1);
+            this.Controls.Add(this.BaconCheeseBurger);
+            this.Controls.Add(this.CheeseBurger);
+            this.Controls.Add(this.PlainBurger);
+            this.Name = "Form1";
+            this.Text = "TheHamburgler ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PlainBurger;
+        private System.Windows.Forms.Label CheeseBurger;
+        private System.Windows.Forms.Label BaconCheeseBurger;
+        private System.Windows.Forms.TextBox box1;
+        private System.Windows.Forms.TextBox box2;
+        private System.Windows.Forms.TextBox box3;
+        private System.Windows.Forms.Label TotalCost;
+        private System.Windows.Forms.Label TaxLabel;
+        private System.Windows.Forms.Label TotalPriceLabel;
+        private System.Windows.Forms.Label PayHere;
+        private System.Windows.Forms.TextBox PayBox;
+        private System.Windows.Forms.Button ChangeCalculator;
+        private System.Windows.Forms.Label Change;
+        private System.Windows.Forms.Button ReciptBotton;
+        private System.Windows.Forms.Button BuyButton;
+        private System.Windows.Forms.Label subtotalLabel;
+        private System.Windows.Forms.Label subtotalOutput;
+        private System.Windows.Forms.Label changeLabel;
+    }
+}
+
---
+++ Cash regestir/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cash_regestir
+{
+    public partial class Form1 : Form
+    {
+        int Burger1 = 4;
+        int Burger2 = 5;
+        int Burger3 = 6;
+        int PlainBox;
+        int CheeseBox;
+        int BaconCheeseBox;
+        int PlainPrice;
+        int CheesePrice;
+        int BaconPrice;
+        double subtotal = 0;
+        double FinalCost;
+        double taxRate = 0.13;
+        double FinalTax;
+        double change;
+        int Payment;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Tax_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BuyItems_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch { }
+
+        }
+
+        private void Change_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BuyButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                PlainBox = Convert.ToInt32(box1.Text);
+                CheeseBox = Convert.ToInt32(box2.Text);
+                BaconCheeseBox = Convert.ToInt32(box3.Text);
+
+                PlainPrice = PlainBox * Burger1;
+                CheesePrice = CheeseBox * Burger2;
+                BaconPrice = BaconCheeseBox * Burger3;
+
+                subtotal = PlainPrice + CheesePrice + BaconPrice;
+                FinalTax = subtotal * taxRate;
+                FinalCost = subtotal + FinalTax;
+
+                subtotalOutput.Text = $"{subtotal.ToString("c")}";
+
+                TaxLabel.Text = $"Tax {FinalTax.ToString("c")}";
+                TotalPriceLabel.Text = $"Final Price {FinalCost.ToString("c")}";
+
+
+
+
+
+
+
+
+
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void ChangeCalculator_Click(object sender, EventArgs e)
+        {
+            TotalPriceLabel.Text = $"Total Price {FinalCost.ToString("C")}";
+            Payment = Convert.ToInt32(PayBox.Text);
+            change = Payment - FinalCost;
+            changeLabel.Text = $"Change {change.ToString("C")}";
+        }
+        
+    }
+}
